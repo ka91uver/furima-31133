d7c62b984ea75a213c4f5fc0ae411e8fb69e7a0a
ka91uver <oh.226.kuoria@outlook.jp>
2020-11-11 19:28:37 +0900

    ユーザー管理機能の実装
    ・ユーザーの新規登録
    ・ログイン/ログアウト機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # before_action :authenticate_user!
+  # protect_from_forgery with: :null_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname , :email , :password , :password_confirmation , :first_name , :last_name , :first_name_kana , :last_name_kana , :birth_date]
+    )
+    # devise_parameter_sanitizer.permit(:sign_in, keys: [:email , :password])
+  end
 end
