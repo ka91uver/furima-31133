58ffa48a00d4632913f4990d1c8eaa7ecbc968c3
ka91uver <oh.226.kuoria@outlook.jp>
2020-11-16 19:41:18 +0900

    ActiveStorageのアソシエーションをモデルに記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,18 +2,22 @@ class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:imdex, :show] 
 
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    Item.create(item_params)
   end
 
   private
 
   def item_params
-    params.require(:item).permit()
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 
   # def move_to_index
   #   unless user_signed_in?
