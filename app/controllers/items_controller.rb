49f2ac3636117fc2ff0956c8c25e3403f8023de4
ka91uver <oh.226.kuoria@outlook.jp>
2020-11-16 17:06:03 +0900

    Itemモデル再設定、ActiveStorage導入
    モデルをdeviseコマンドで作成してしまったので削除し、作り直した。
    ActiveStorageを導入しアソシエーションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,19 @@
 class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:imdex, :show] 
 
-  # def index
-  # end
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit()
 
   # def move_to_index
   #   unless user_signed_in?
