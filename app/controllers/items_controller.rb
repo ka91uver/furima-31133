97e230da59cffc74de65a55a4c67371bb23d02c5
ka91uver <oh.226.kuoria@outlook.jp>
2020-11-17 19:21:02 +0900

    データベースに保存できるように記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  # before_action :move_to_index, except: [:imdex, :show] 
+  before_action :authenticate_user!, only: [:new] 
 
   def index
     @items = Item.all
@@ -10,13 +10,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :item_description, :category_id, :condition_id, :shipping_fee_id, :ship_from_id, :days_until_shipping_id, :price, :image).merge(user_id: current_user.id)
   end
 
   # def move_to_index
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_fee_id
   belongs_to_active_hash :ship_from_id
   belongs_to_active_hash :days_until_shipping_id
-  # belongs_to :price
+  belongs_to_active_hash :price
 
-  validates :content, presence: true
+  validates :item_name, :item_description, :price, presence: true
   validates :category_id, :condition_id, :shipping_fee_id, :ship_from_id, :days_until_shipping_id, numericality: { other_than: 1 }
 end
