f738967fd4ca887e28916ebbdb1dac2be100d18d
ka91uver <oh.226.kuoria@outlook.jp>
2020-11-17 17:16:21 +0900

    ActiveHashを導入,バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,14 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category_id
+  belongs_to_active_hash :condition_id
+  belongs_to_active_hash :shipping_fee_id
+  belongs_to_active_hash :ship_from_id
+  belongs_to_active_hash :days_until_shipping_id
+  # belongs_to :price
+
   validates :content, presence: true
+  validates :category_id, :condition_id, :shipping_fee_id, :ship_from_id, :days_until_shipping_id, numericality: { other_than: 1 }
 end
