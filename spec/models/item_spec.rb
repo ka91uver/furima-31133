70ed3447e0a61a231c260f8b5ae7cbde2a282a9f
ka91uver <oh.226.kuoria@outlook.jp>
2020-11-18 19:52:32 +0900

    Itemモデルの単体テストコード修正
    不要ファイルの削除、数値には''(シングルクォテーション)がいらないので外す。

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,11 +10,6 @@ RSpec.describe Item, type: :model do
       it 'item_name、item_description、category_id、condition_id、shipping_fee_id、ship_from_id、days_until_shipping_id、priceが存在すれば登録できる' do
         expect(@item).to be_valid
       end
-
-      it 'Priceが半角数字で¥300~¥9,999,999の間であれば登録できる' do
-        @item.price = '500'
-        expect(@item).to be_valid
-      end
     end
 
     context '出品登録が上手くいかないとき' do
@@ -40,43 +35,43 @@ RSpec.describe Item, type: :model do
       end
 
       it 'category_idが1以下だと登録できない' do
-        @item.category_id = ' 1 '
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it 'condition_idが1以下だと登録できない' do
-        @item.condition_id = ' 1 '
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition must be other than 1")
       end
       it 'shipping_fee_idが1以下だと登録できない' do
-        @item.shipping_fee_id = ' 1 '
+        @item.shipping_fee_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee must be other than 1")
       end
       it 'ship_from_idが1以下だと登録できない' do
-        @item.ship_from_id = ' 1 '
+        @item.ship_from_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Ship from must be other than 1")
       end
       it 'days_until_shipping_idが1以下だと登録できない' do
-        @item.days_until_shipping_id = ' 1 '
+        @item.days_until_shipping_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Days until shipping must be other than 1")
       end
 
       it 'priceが¥300未満だと登録できない' do
-        @item.price = "200"
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it 'priceが¥9,999,999より大きいと登録できない' do
-        @item.price = "10000000"
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
       it 'priceが半角数字でないと登録できない' do
-        @item.price = "５００"
+        @item.price = ' ５００ '
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
