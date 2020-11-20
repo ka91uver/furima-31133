7b8fa69286aebe17d18ba6be240cbc36786f66ad
ka91uver <oh.226.kuoria@outlook.jp>
2020-11-20 15:59:55 +0900

    purchase_adressのテストコード追記

--- spec/factories/purchase_adresses.rb
+++ spec/factories/purchase_adresses.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :purchase_adress do
-    post_code { '123-4567' }
+    post_code     { '123-4567' }
     prefecture_id { 2 }
-    city { 港区 }
-    house_number { 六本木 }
+    city          { 港区 }
+    house_number  { 六本木 }
     building_name { 六本木ユニハウス }
-    phone_number { 12345678900 }
+    phone_number  { 12345678900 }
   end
 end
--- spec/models/purchase_adress_spec.rb
+++ spec/models/purchase_adress_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseAdress, type: :model do
-  describe '購入情報の保存' do
-    before do
-      @purchase_adress = FactoryBot.build(:purchase_adress)
-    end
+  before do
+    @purchase_adress = FactoryBot.build(:purchase_adress)
+  end
 
-    const '商品を購入できるとき' do
+  describe '購入情報の保存' do
+    context '商品を購入できるとき' do
       it 'すべての値が正しく入力されていれば保存できること' do
         expect(@purchase_adress).to be_valid
       end
-
       it 'building_nameは空でも保存できること' do
         @purchase_adress.building_name = nil
         expect(@purchase_adress).to be_valid
       end
     end
 
-    const '商品を購入できないとき' do
+    context '商品を購入できないとき' do
       it 'post_codeが空では保存できないこと' do
         @purchase_adress.postal_code = nil
         @purchase_adress.valid?
@@ -66,7 +65,7 @@ RSpec.describe PurchaseAdress, type: :model do
         expect(@purchase_adress.errors.full_messages).to include("Phone number is invalid")
       end
 
-      it 'phone_numberが12文字以上では保存できないこと'
+      it 'phone_numberが12文字以上では保存できないこと' do
         @purchase_adress.phone_number = '123456789098'
         @purchase_adress.valid?
         except(@purchase_adress.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)")
