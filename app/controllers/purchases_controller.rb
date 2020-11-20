18f7ecc0d1a5e8f689a3782eae54f640379d8b62
ka91uver <oh.226.kuoria@outlook.jp>
2020-11-20 15:03:03 +0900

    Formオブジェクトを使用
    購入情報と配達先情報をまとめた

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,22 @@
 class PurchasesController < ApplicationController
+  def index
+    @purchase_adress = PurchaseAdress.new
+  end
+
+  def create
+    @purchase_adress = PurchaseAdress.new(adress_params)
+    if @purchase_adress.valid?
+        @purchase_adress.save
+        redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def adress_params
+    params.require(:purchase_adress).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+  
 end
