251626c4db1a43aaf6c034cbd0bdcdbe609f46c9
ka91uver <oh.226.kuoria@outlook.jp>
2020-11-20 11:58:46 +0900

    削除機能の実装.修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show, :new, :create, :destroy]
   before_action :item_collect, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, except: [:index, :show, :new, :create, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -51,12 +51,12 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def item_params
-    params.require(:item).permit(:item_name, :item_description, :category_id, :condition_id, :shipping_fee_id, :ship_from_id, :days_until_shipping_id, :price, :image).merge(user_id: current_user.id)
-  end
-
   def move_to_index
     @item = Item.find(params[:id])
     redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id
+  end
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_description, :category_id, :condition_id, :shipping_fee_id, :ship_from_id, :days_until_shipping_id, :price, :image).merge(user_id: current_user.id)
   end
 end
